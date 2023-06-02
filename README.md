47207bf0c5191b85311b6119a5ef837e732f03d6
Sergi Lipovyi <777lsg@gmail.com>
2023-06-02 13:56:03 +0000

    SF_B9.5.1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SF_B9.5.1
+- SF_B9.5.1
+- npm i lsgsfb951
+
---
+++ proj01.js
@@ -0,0 +1,26 @@
+let arrOne = [
+
+    'Первый',
+    'Второй',
+    'Третий'
+];
+
+
+let matrix = [
+
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+
+console.log(matrix)
+console.log(arrOne)
+console.log(matrix[1][2])
+console.log(arrOne[0])
+
+
+var array = [1, 2, 3, 4, 5];
+array.forEach(function (element) {
+    console.log(element);
+});
